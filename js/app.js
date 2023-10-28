4e7bfce33f1799f1b1af4ecb172a5e2aa063e57a
cyeguez <cesar.yeguez@gmail.com>
2023-10-28 12:59:49 -0300

    se generan estilos proyecto se esta maquetando dinamicamente

---
+++ js/app.js
@@ -0,0 +1,59 @@
+import data from "../asset/data.json" assert { type: "json" };
+const containerBar = document.querySelector(".statistics__body");
+let arrayValues=[];
+let arrayAux = [];
+data.forEach((item) => {
+  const template = 
+  ` <div class="statistics__body__item">
+      <div class="statistics__body__item__bar">
+        <p class="statistics__body__item__label">${item.day}</p>
+        <p class="statistics__body__item__value">$${item.amount}</p>
+      </div>
+    </div>
+  `
+  containerBar.innerHTML += template;
+  arrayValues.push( item.amount); 
+  
+ 
+ 
+  
+});
+
+console.log(arrayValues)
+
+
+let valueMax = Math.max(...arrayValues );
+console.log(valueMax)
+
+let bars = document.querySelectorAll(".statistics__body__item__bar");
+bars = [...bars ];//transformamos en nodo en un array
+
+let price = document.querySelector(".statistics__body__item__value")
+bars.forEach((bar) => {
+ 
+ let valueBar = bar.childNodes[3].innerText;
+ valueBar = valueBar.replace("$", "");
+ 
+
+ let hightBar = Math.trunc((valueBar * 150)/valueMax);
+ console.log((hightBar));
+ bar.style.height = `${hightBar}px`;
+ 
+
+  
+  
+ 
+
+  bar.addEventListener("mouseover", (e) => {
+  
+    let value= e.target.childNodes[3];
+    value.style.display = "block"
+    
+  });
+
+  bar.addEventListener("mouseout", (e) => {
+    
+    let value= e.target.childNodes[3];
+    value.style.display= "none";
+  });
+});
